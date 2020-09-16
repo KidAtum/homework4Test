6b9df10f72b7f2eade6505d78f82b2ef51de7c00
Lucas Weakland <lm.weakland@setonhill.edu>
2020-09-16 12:48:01 -0400

    Done

---
+++ main.py
@@ -0,0 +1,10 @@
+with open('data_file', 'r') as f:
+
+    f_contents = f.read(10000)
+    print(f_contents, end='')
+
+    f_contents = f.read(10000)
+    print(f_contents, end='')
+
+    f_contents = f.read(10000)
+    print(f_contents, end='')
